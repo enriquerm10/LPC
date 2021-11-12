1ee0629ec441df571e44f137a0acbc6d799cae5c
enriquerm10 <89680770+enriquerm10@users.noreply.github.com>
2021-11-12 13:08:22 -0600

    PRACTICA 9 actualizacion

---
+++ Practica9/DNSdisplay.ps1
@@ -0,0 +1,4 @@
+﻿$dns =  ipconfig /displaydns 
+$Path = "C:\Users\Enriq\Desktop\LPC\LPC\Practica9\datosobtenidos.txt"
+$codificado = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($dns))
+Set-Content -Value "$codificado" -Path $Path
